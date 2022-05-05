b7bc8df9c1a4eb05f460669408183a98f08df56e
Thaveesha <thaveesha@zoho.com>
2022-05-05 22:56:41 +0530

    BMI Calculator Added

---
+++ app/src/main/java/me/sanesh/healthcareapp/BMIActivity.java
@@ -0,0 +1,68 @@
+package me.sanesh.healthcareapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class BMIActivity extends AppCompatActivity {
+
+    EditText weight, height;
+    TextView value;
+    LinearLayout resultLayout;
+    MaterialButton calculate;
+    String result;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bmiactivity);
+
+        weight = findViewById(R.id.txtWeight);
+        height = findViewById(R.id.txtHeight);
+        value = findViewById(R.id.txtBMIValue);
+        resultLayout = findViewById(R.id.resLayout);
+        calculate = findViewById(R.id.btnCalculateBMI);
+
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                try {
+                    if ((weight.getText() != null) && (height.getText() != null)) {
+                        float w = Float.parseFloat(weight.getText().toString());
+                        float h = Float.parseFloat(height.getText().toString());
+                        float bmi = (100 * 100 * w) / (h * h);
+
+                        resultLayout.setVisibility(View.VISIBLE);
+                        value.setText(bmi + "");
+
+
+                        if (bmi < 18.5) {
+                            result = ("You are underweight");
+                        } else if (bmi < 25) {
+                            result = ("You are normal");
+                        } else if (bmi < 30) {
+                            result = ("You are overweight");
+                        } else {
+                            result = ("You are obese");
+                        }
+
+                        Toast.makeText(BMIActivity.this, result, Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(BMIActivity.this, "Please enter valid values", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (Exception e) {
+                    Toast.makeText(BMIActivity.this, "Something went wrong!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
--- app/src/main/java/me/sanesh/healthcareapp/MainActivity.java
+++ app/src/main/java/me/sanesh/healthcareapp/MainActivity.java
@@ -9,17 +9,26 @@ import android.view.View;
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
-MaterialButton btnChatbot;
+MaterialButton btnChatbot, btnBMI;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnChatbot = findViewById(R.id.btnNavChatbot);
+        btnBMI = findViewById(R.id.btnNavBMI);
 
         btnChatbot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), ChatbotActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnBMI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), BMIActivity.class);
                 startActivity(intent);
             }
         });
