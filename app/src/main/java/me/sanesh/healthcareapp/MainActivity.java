a409a2d861a2d18bcbec07102b3f4b885053b768
Malindu Nawarathne <ransaranawarathne@gmail.com>
2022-05-05 22:50:27 +0530

    Location Share Branch initial commit

--- app/src/main/java/me/sanesh/healthcareapp/MainActivity.java
+++ app/src/main/java/me/sanesh/healthcareapp/MainActivity.java
@@ -9,17 +9,26 @@ import android.view.View;
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
-MaterialButton btnChatbot;
+MaterialButton btnChatbot, btnShareLocation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnChatbot = findViewById(R.id.btnNavChatbot);
+        btnShareLocation = findViewById(R.id.btnNavCallAmbulance);
 
         btnChatbot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), ChatbotActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnShareLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LocationShareActivity.class);
                 startActivity(intent);
             }
         });
