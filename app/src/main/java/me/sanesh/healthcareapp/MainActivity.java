48aea1676e1d088cf20372be62e8945ed2e3bc15
Thaveesha <thaveesha@zoho.com>
2022-05-06 00:22:14 +0530

    Call Ambulance Added

--- app/src/main/java/me/sanesh/healthcareapp/MainActivity.java
+++ app/src/main/java/me/sanesh/healthcareapp/MainActivity.java
@@ -1,21 +1,29 @@
 package me.sanesh.healthcareapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
-MaterialButton btnChatbot, btnBMI;
+    static int PERMISSION_CODE = 100;
+    MaterialButton btnChatbot, btnBMI, btnCall;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnChatbot = findViewById(R.id.btnNavChatbot);
         btnBMI = findViewById(R.id.btnNavBMI);
+        btnCall = findViewById(R.id.btnNavCallAmbulance);
 
         btnChatbot.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +37,19 @@ MaterialButton btnChatbot, btnBMI;
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), BMIActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext().getApplicationContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED)
+            ;
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, PERMISSION_CODE);
+
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:" + 1919));
                 startActivity(intent);
             }
         });
